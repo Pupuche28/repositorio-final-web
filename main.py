fb435c91ee1ce11dd827c6a3122fc520dfa73136
Pupuche28 <153439953+Pupuche28@users.noreply.github.com>
2024-12-06 17:38:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,68 @@
-from flask import Flask, render_template, redirect, url_for
+from functools import wraps
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
 from flask_login import LoginManager, login_required, current_user
-from routes.usuario_routes import usuario_bp  # Importar el blueprint de usuario
-from routes.autenticacion_routes import autenticacion_bp  # Importar el blueprint de autenticación
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+from routes.usuario_routes import usuario_bp
+from routes.autenticacion_routes import autenticacion_bp
 from routes.admin_routes import admin_bp
 from routes.rol_routes import rol_bp
-from routes.resenas_routes import resena_bp  # Importar el blueprint de reseñas
+from routes.resenas_routes import resena_bp
 from routes.categoria_routes import categoria_bp
-from routes.producto_routes import producto_bp  # Importar el blueprint de productos
+from routes.producto_routes import producto_bp
 from routes.ceramica_routes import ceramica_bp
 from routes.joyeria_routes import joyeria_bp
 from routes.retablos_routes import retablos_bp
 from routes.textiles_routes import textiles_bp
 from routes.detalleproducto_routes import detalle_producto_bp
-from routes.carrito_routes import carrito_bp  # Importar el blueprint del carrito
+from routes.carrito_routes import carrito_bp
 from routes.tipoentrega_routes import tipo_entrega_bp
 from routes.tipopago_routes import tipopago_bp
 from routes.mispedidos_routes import mispedidos_bp
 from routes.tarjetasAdmin_routes import tarjetaAdmin_bp
 from routes.pedido_routes import pedido_bp
-
+from routes.pago_routes import pago_bp
+from routes.detalleAdmin_routes import detalleAdmin_bp
 
 from controllers.controlador_ceramica import obtener_productos_ceramica
 import controllers.controlador_tipoentrega as controlador_tipoentrega
 from controllers.controlador_detalleproducto import obtener_producto_por_id
-
-import controllers.controlador_resenas as controlador_resenas  
-import controllers.controlador_usuarios as controlador_usuarios  # Controlador para usuarios
-
-
+import controllers.controlador_resenas as controlador_resenas
+import controllers.controlador_usuarios as controlador_usuarios
+import hashlib
+from controllers.controlador_usuarios import obtener_usuario_por_email
 
 app = Flask(__name__)
 
 # Agregar una clave secreta para las sesiones
-app.secret_key = 'rodrigho'  # Clave secreta para las sesiones
+app.secret_key = 'rodrigho'
+
+# Configuración para Flask-JWT-Extended
+app.config['JWT_SECRET_KEY'] = 'mi_clave_secreta_super_segura'
+jwt = JWTManager(app)
 
 # Configurar Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'autenticacion_bp.login'  # Redirigir a la página de login si no ha iniciado sesión
-
+login_manager.login_view = 'autenticacion_bp.login'
 
 # Función para cargar usuario desde el ID de sesión
 @login_manager.user_loader
 def load_user(user_id):
     return controlador_usuarios.obtener_usuario_por_id(user_id)
 
-
-
 # Context processor: Hacer disponible `current_user` en todas las plantillas
 @app.context_processor
 def inject_user():
     return dict(current_user=current_user)
 
-# Registrar el blueprint de usuario y autenticación
+# Registrar los blueprints
 app.register_blueprint(usuario_bp, url_prefix="/usuario")
-app.register_blueprint(autenticacion_bp, url_prefix="/auth")  # Registrar las rutas de autenticación
+app.register_blueprint(autenticacion_bp, url_prefix="/auth")
 app.register_blueprint(admin_bp, url_prefix="/admin")
 app.register_blueprint(rol_bp, url_prefix="/rol")
 app.register_blueprint(categoria_bp, url_prefix="/categoria")
-app.register_blueprint(resena_bp, url_prefix="/resena")  # Registrar las rutas de reseñas
+app.register_blueprint(resena_bp, url_prefix="/resena")
 app.register_blueprint(producto_bp, url_prefix='/producto')
 app.register_blueprint(ceramica_bp)
 app.register_blueprint(joyeria_bp)
@@ -71,130 +75,188 @@ app.register_blueprint(tipopago_bp)
 app.register_blueprint(mispedidos_bp)
 app.register_blueprint(tarjetaAdmin_bp, url_prefix='/tarjetasAdmin')
 app.register_blueprint(pedido_bp, url_prefix='/pedidos')
+app.register_blueprint(pago_bp, url_prefix='/pago')
+app.register_blueprint(detalleAdmin_bp, url_prefix='/detallePedido')
 
-# Redirigir a la página de administrador si el usuario tiene rol de administrador
+# Decorador para verificar que el usuario es administrador
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated and current_user.idRol == 1:  # 1 es el idRol para "administrador"
+            return f(*args, **kwargs)
+        else:
+            flash("Acceso denegado. Solo los administradores pueden acceder a esta página.", "danger")
+            return redirect(url_for("home"))
+    return decorated_function
+
+# Rutas para el home
 @app.route("/")
 def home():
-    # Verificar si el usuario está autenticado
     if current_user.is_authenticated:
-        # Si el usuario es administrador (idRol = 1)
-        if current_user.idRol == 1:  # 1 es el idRol para "administrador"
-            return render_template("menuAdministrador.html")  # Redirigir al menú del administrador
+        if current_user.idRol == 1:
+            return render_template("menuAdministrador.html")
         else:
-            return render_template("index.html")  # Si no es administrador, mostrar el home normal
+            return render_template("index.html")
     else:
-        return render_template("index.html")  # Si no está autenticado, mostrar el home normal
+        return render_template("index.html")
 
-# Página del administrador
+# Rutas protegidas para administradores
 @app.route("/admin")
-@login_required
+@admin_required
 def admin_home():
-    # Verifica que el usuario sea administrador antes de mostrar la página
-    if current_user.idRol == 1:  # 1 es el rol de administrador
-        return render_template("menuAdministrador.html")
-    else:
-        return redirect(url_for("home"))  # Si no es administrador, redirigir a la página principal
+    return render_template("menuAdministrador.html")
 
-# Ruta para la página "cambios.html"
+@app.route("/producto/menu_man_producto")
+@admin_required
+def menu_man_producto():
+    return render_template("menu_man_producto.html")
+
+@app.route("/usuario/menu_man_usuario")
+@admin_required
+def menu_man_usuario():
+    return render_template("menu_man_usuario.html")
+
+@app.route("/rol/menu_man_rol")
+@admin_required
+def menu_man_rol():
+    return render_template("menu_man_rol.html")
+
+@app.route("/categoria/menu_man_categoria")
+@admin_required
+def menu_man_categoria():
+    return render_template("menu_man_categoria.html")
+
+@app.route("/resena/menu_man_resena")
+@admin_required
+def menu_man_resena():
+    return render_template("menu_man_resena.html")
+
+@app.route("/tarjetasAdmin/menu_man_tarjeta")
+@admin_required
+def menu_man_tarjeta():
+    return render_template("menu_man_tarjeta.html")
+
+@app.route("/pedidos/menu_man_pedido")
+@admin_required
+def menu_man_pedido():
+    return render_template("menu_man_pedido.html")
+
+@app.route("/pago/menu_man_pago")
+@admin_required
+def menu_man_pago():
+    return render_template("menu_man_pago.html")
+
+@app.route("/admin/menu_administrador")
+@admin_required
+def menu_administrador():
+    return render_template("menu_administrador.html")
+
+
+# Ruta para el login y la autenticación
+@app.route('/obtener_tokem', methods=['POST'])
+def login():
+    try:
+        email = request.json.get('email', None)
+        contrasena = request.json.get('contrasena', None)
+
+        if not email or not contrasena:
+            return jsonify({"message": "Correo y contraseña son obligatorios"}), 400
+
+        usuario = obtener_usuario_por_email(email)
+        if usuario:
+            if usuario.idRol != 1:
+                return jsonify({"message": "Acceso denegado. Solo los administradores pueden generar tokens."}), 403
+
+            contrasena_hashed = hashlib.sha256(contrasena.encode('utf-8')).hexdigest()
+            if usuario.contrasena == contrasena_hashed:
+                access_token = create_access_token(identity=str(usuario.id))
+                return jsonify({"token": access_token, "usuario": {"id": usuario.id, "nombres": usuario.nombres}}), 200
+            else:
+                return jsonify({"message": "Contraseña incorrecta"}), 401
+        else:
+            return jsonify({"message": "Usuario no encontrado"}), 404
+    except Exception as e:
+        return jsonify({"message": "Error interno del servidor", "error": str(e)}), 500
+
+# Rutas protegidas con autenticación
 @app.route("/cambios")
 def cambios():
     return render_template("cambios.html")
 
-# Ruta para la página "carrito.html"
 @app.route("/carrito")
 @login_required
 def carrito():
     return render_template("carrito.html")
 
-# Ruta para la página "ceramica.html"
 @app.route('/ceramica')
 def ceramica():
-    productos_ceramica = obtener_productos_ceramica()  # Obtiene los productos de cerámica
+    productos_ceramica = obtener_productos_ceramica()
     return render_template('ceramica.html', productos=productos_ceramica)
 
-
-# Ruta para la página "contactos.html"
 @app.route("/contactos")
 def contactos():
     return render_template("contactos.html")
 
-# Ruta para la página "joyeria.html"
 @app.route("/joyeria")
 def joyeria():
     return render_template("joyeria.html")
 
-# Ruta para la página "legales.html"
 @app.route("/legales")
 def legales():
     return render_template("legales.html")
 
-# Ruta para la página "nosotros.html"
 @app.route("/nosotros")
 def nosotros():
     resenas = controlador_resenas.obtener_todas_resenas()
     return render_template("nosotros.html", resenas=resenas)
 
-# Ruta para la página "politicas.html"
 @app.route("/politicas")
 def politicas():
     return render_template("politicas.html")
 
-# Ruta para la página "preguntas.html"
 @app.route("/preguntas")
 def preguntas():
     return render_template("preguntas.html")
 
-# Ruta para la página "productodetalle.html"
 @app.route('/producto/<int:id_producto>')
 def productodetalle(id_producto):
-    # Obtener el producto por su ID
     producto = obtener_producto_por_id(id_producto)
-    
-    # Verificar si el producto fue encontrado
     if producto:
-        # Pasar el producto a la plantilla
         return render_template('productodetalle.html', producto=producto)
     else:
-        # Manejar el caso en el que el producto no existe
         return "Producto no encontrado", 404
 
-
-# Ruta para la página "registrate.html"
 @app.route("/registrate")
 def registrate():
     return render_template("registrate.html")
 
-# Ruta para la página "retablos.html"
 @app.route("/retablos")
 def retablos():
     return render_template("retablos.html")
 
-# Ruta para la página "terminos.html"
 @app.route("/terminos")
 def terminos():
     return render_template("terminos.html")
 
-# Ruta para la página "textiles.html"
 @app.route("/textiles")
 def textiles():
     return render_template("textiles.html")
 
-# Ruta para la página "tipoentrega.html"
 @app.route("/tipoentrega")
-@login_required  # Asegurarse de que el usuario esté autenticado
+@login_required
 def tipoentrega():
-    # Obtener la dirección del usuario desde el controlador
     direccion = controlador_tipoentrega.obtener_direccion_usuario(current_user.id)
-
-    # Renderizar la plantilla pasando la dirección del usuario
     return render_template("tipoentrega.html", direccion=direccion)
 
-
-# Ruta para la página "tipopago.html"
 @app.route("/tipopago")
 def tipopago():
     return render_template("tipopago.html")
 
+# Manejar el caso de rutas no autorizadas
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash("Debes iniciar sesión para acceder a esta página.", "warning")
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
